9c21e474ad3a5dbb3565b3c176640945dd90b7ed
angelorapana <angelorapana@icloud.com>
2026-02-18 11:38:43 +0100

    first commit

---
+++ react-use-state/src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Header } from './assets/Components/Header';
+
+function App() {
+
+  return (
+    <Header />
+  )
+}
+
+export default App
---
+++ react-use-state/src/assets/Components/Header.jsx
@@ -0,0 +1,8 @@
+
+export function Header() {
+    return (
+        <div className="container bg-light p-3">
+            <h1> <b> LEARN WEB DEVELOMPMENT</b></h1>
+        </div>
+    )
+}
